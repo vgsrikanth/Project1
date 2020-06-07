36d7733dc2abd855d2cc52cce19b14886832e207
vgsrikanth <sgv.usa@gmail.com>
2020-06-07 17:44:16 -0400

    Added Comment to test cloning of remote respository

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,5 @@
 // Added this comment from vgs-usa which is forked version of the project Project1 from vgsrikanth
+// I am adding a new comment to test the cloning of my repository
 public class Calculator {
 	// Added Comment
     public static main(String args[]) {
